5bef0d23696bef6a9cd161c36cac7ddcc740c37f
Mateusz <matzaj101@gmail.com>
2024-07-17 12:36:58 +0200

    fix payment sdervice test

--- payment/service_test.go
+++ payment/service_test.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"testing"
 
+	"github.com/matizaj/oms/common/discovery/inmemory"
 	pb "github.com/matizaj/oms/common/proto"
+	"github.com/matizaj/oms/payment/gateway"
 	testprocessor "github.com/matizaj/oms/payment/processors/test_processor"
 )
 
 func TestSetvice(t *testing.T) {
 	tprocessor := testprocessor.NewTestProcessor()
-	service := NewPaymentService(tprocessor)
+	inmem_registry := inmemory.NewRegistry()
+	gtw := gateway.NewPaymentGateway(inmem_registry)
+	service := NewPaymentService(tprocessor, gtw)
 	
 	t.Run("should create a paymewnt link", func(t *testing.T) {
 		dymmyOrder := &pb.CreateOrderResponse{}
